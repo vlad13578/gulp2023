cf69d83176f4964c540800ec659cebfd65ae5bff
vlad13578 <13578oo8@gmail.com>
2023-07-18 21:41:25 +0300

    Закінчене налаштування scss

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -2,6 +2,11 @@ import dartSass from "sass";
 import gulpSass from "gulp-sass";
 import rename from "gulp-rename";
 
+import cleanCss from "gulp-clean-css";
+import webpcss from "gulp-webpcss";
+import autoprefixer from "gulp-autoprefixer";
+import groupCssMediaQueries from "gulp-group-css-media-queries";
+
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
@@ -16,7 +21,22 @@ export const scss = () => {
         )
         .pipe(app.plugins.replace(/@img\//g, "../img/"))
         .pipe(sass({ outputstyle: `expanded` }))
-        .pipe(rename({ extname:'.min.css' }))
+        .pipe(groupCssMediaQueries())
+        .pipe(webpcss(
+            {
+                webpClass: ".webp",
+                noWebpClass: ".no-webp",
+            }
+        ))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserslist: ["last 3 versions"],
+            cascade: true,
+        }))
+        //Розкоментувати якщо потрібен не стислий дубль файлу стилів
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
+        .pipe(rename({ extname: ".min.css" }))
         .pipe(app.gulp.dest(app.path.build.css))
         .pipe(app.plugins.browsersync.stream());
 };

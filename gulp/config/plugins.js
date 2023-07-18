050ecf8481e476f2a506bfd97daedc1117ebda0c
vlad13578 <13578oo8@gmail.com>
2023-07-18 19:03:53 +0300

    readme + сповіщення

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,10 @@
 import replace from "gulp-replace"; //Пошук та заміна
+import plumber from "gulp-plumber"; //Обробка помилок
+import notify from "gulp-notify"; //Повідомлення (підказки)
 
 //Експортування об'єктів
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
 }

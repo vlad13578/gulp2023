9ba7bbcc02551798f89fbe805002aebea4e6e0b3
vlad13578 <13578oo8@gmail.com>
2023-07-19 13:46:59 +0300

    + оптимізація картинок /webp

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,6 +2,7 @@ import replace from "gulp-replace"; //Пошук та заміна
 import plumber from "gulp-plumber"; //Обробка помилок
 import notify from "gulp-notify"; //Повідомлення (підказки)
 import browsersync from "browser-sync"; //Локальний сервер
+import newer from "gulp-newer"; //Перевірка обновлень
 
 //Експортування об'єктів
 export const plugins = {
@@ -9,4 +10,5 @@ export const plugins = {
     plumber: plumber,
     notify: notify,
     browsersync: browsersync,
+    newer:newer,
 }
--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,7 @@ import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
 
 //Спостерігач за змінами в файлах
 function watcher(){
@@ -26,9 +27,10 @@ function watcher(){
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, scss);
     gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js);
+const mainTasks = gulp.parallel(copy, html, scss, js, images);
 
 // Побудова сценаріїв виконання завдань
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
